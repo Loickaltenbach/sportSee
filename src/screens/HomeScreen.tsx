d1551bb1df2f033b57e708162b32fc80a04ebc8d
Loickaltenbach <loickaltenbach@icloud.com>
2023-01-23 16:43:57 +0100

    add: test

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,11 +3,9 @@ import getInstance from '../services/services';
 
 const HomeScreen = () => {
     const user = getInstance.GetUserDataById(18);
-    const activity = getInstance.GetUserActivityById(18);
     return (
         <div>
-            <p>{user?.score}</p>
-            <p>{activity?.sessions[0].day}</p>
+            <p>{user?.id}</p>
         </div>
     );
 }
